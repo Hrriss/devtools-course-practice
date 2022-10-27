13c9ed3c5f3c61966da1613c04ea6a924466ca1b
dtaleme <kotm2002@mail.ru>
2022-10-27 15:49:45 +0300

    fix coverage

--- modules/khristina_motyl_simple_nums/include/simple_num.h
+++ modules/khristina_motyl_simple_nums/include/simple_num.h
@@ -6,8 +6,8 @@
 
 class simple_numbers {
  public:
-    static std::vector<int> _Calculate(int start, int end);
-    static void _Print(int start, int end);
+  static std::vector<int> _Calculate(int start, int end);
+  static void _Print(int start, int end);
 };
 
 #endif  // MODULES_KHRISTINA_MOTYL_SIMPLE_NUMS_INCLUDE_SIMPLE_NUM_H_
